b0a039a64b9fb5f0d7514174f0e1067814304d51
Jesús Amieiro <amieiro@gmail.com>
2019-12-14 17:53:00 +0000

    Añado el menú principal obtenido a través de la API REST de WordPress

--- app/Http/Controllers/WordpressController.php
+++ app/Http/Controllers/WordpressController.php
@@ -2,11 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
+
 
 class WordpressController extends Controller
 {
+    private $url = 'http://wordpress.test/wp-json/wp/v2/';
+
     public function index() {
-        return view('index');
+        $cliente = new Client();
+        $response = $cliente->get($this->url . 'menu');
+        $menu = json_decode($response->getBody());
+        return view('index', compact('menu'));
     }
 }
